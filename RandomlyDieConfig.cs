c2745b5dd9b3d30ef2a11180dcc46488dce73bff
Potat369 <yevheniidemian@gmail.com>
2024-11-20 12:41:16 +0200

    Add config for chance

---
+++ RandomlyDieConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace RandomlyDie;
+
+public class RandomlyDieConfig : ModConfig
+{
+    public override ConfigScope Mode => ConfigScope.ServerSide;
+    
+    [DefaultValue(480)]
+    [Range(1, int.MaxValue)]
+    public int Chance;
+}
