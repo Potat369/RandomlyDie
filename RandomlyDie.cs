36f2a9916d370c6eb6b9085d2b0fd5d9ef00a6b6
Potat369 <yevheniidemian@gmail.com>
2024-11-20 13:11:41 +0200

    Add kill logic

--- RandomlyDie.cs
+++ RandomlyDie.cs
@@ -1,9 +1,28 @@
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Animations;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace RandomlyDie
 {
-	public class RandomlyDie : Mod
-	{
+	public class RandomlyDie : Mod;
 
+	public class RandomlyDieSystem : ModSystem
+	{
+		private int ticksPassed;
+		public override void PreUpdatePlayers()
+		{
+			ticksPassed++;
+			if (ticksPassed != 60) return;
+			ticksPassed = 0;
+			int chance = ModContent.GetInstance<RandomlyDieConfig>().Chance;
+			foreach (var player in Main.ActivePlayers)
+			{
+				if (player.dead) continue;
+				if (!Main.rand.NextBool(chance)) continue;
+				player.KillMe(PlayerDeathReason.LegacyEmpty(), 0, 0);
+			}
+		}
 	}
 }
